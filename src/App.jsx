bae92993421fc457b18d788246323547cfc5eb2d
austingw <senortinman@gmail.com>
2022-10-22 16:28:13 -0400

    Add header and site title styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import { useState } from "react";
+import github from "./assets/github.svg";
+import Typewriter from "typewriter-effect";
 
 function App() {
   return (
     <div className="App">
-      <h1>Austin G Williams</h1>
+      <div className="site-title">
+        <h1>
+          <Typewriter
+            options={{
+              strings: ["Austin<br/>Williams<br/>Frontend<br/>Developer"],
+              pauseFor: 150000,
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </h1>
+        <button className="header-btn">
+          Learn More <br />
+          <i className="arrow down"></i>
+        </button>
+      </div>
+      <p>Here's an example of my intro</p>
+      <a href="">Link</a>
+      <i class="devicon-github-original"></i>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <div className="footer">
+      <span>© Copyright {new Date().getFullYear()}, Austin Williams</span>
+      <span>
+        <a href="">View this portfolio on GitHub</a>
+      </span>
+    </div>
+  );
+}
