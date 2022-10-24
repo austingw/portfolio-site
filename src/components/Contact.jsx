616e23f28683f927af723d243b139a67dab214ca
austingw <senortinman@gmail.com>
2022-10-24 12:25:44 -0400

    Add state for contact form submission status and thank you message that displays upon submitting the form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import ContactForm from "./ContactForm";
 
 export default function Contact() {
+  const [submitted, setSubmitted] = useState(false);
+  function handleSubmit() {
+    setSubmitted(true);
+  }
   return (
     <div>
       <h2>Connect</h2>
-      <ContactForm />
+      {submitted ? (
+        <p className="thanks-message">
+          Thanks for reaching out! I will get back to you shortly.
+        </p>
+      ) : (
+        <ContactForm handleSubmit={handleSubmit} />
+      )}
     </div>
   );
 }
