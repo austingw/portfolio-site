f407391cd7f2ab1df4269697c28cbf67fc576205
austingw <senortinman@gmail.com>
2022-10-26 11:58:01 -0400

    Add alternating styling for portfolio elements

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,19 +4,35 @@ export default function Portfolio() {
   return (
     <div className="portfolio">
       <h2>Portfolio</h2>
-      <div className="portfolio-item">
-        <div className="project-summary">
-          <h3>Project: Quiz App</h3>
-          <b>Summary</b>
-          <p>A quiz game made with React that integrates the Open Quiz API</p>
-          <button>Demo</button>
-          <button>Code</button>
-        </div>
+      <div className="portfolio-item-reverse">
         <div className="project-display">
           <img src={quiz} className="project-img right-img" />
         </div>
+        <div className="project-summary">
+          <h3>Client: Greenbrier Church</h3>
+          <p>
+            <b>Summary:</b> A quiz game made with React that integrates the Open
+            Quiz API
+          </p>
+          <button>Demo</button>
+          <button>Code</button>
+        </div>
       </div>
-      <div className="portfolio-item-reverse"></div>
+
+      <div className="portfolio-item">
+        <div className="project-display">
+          <img src={quiz} className="project-img left-img" />
+        </div>
+        <div className="project-summary">
+          <h3>Project: Quiz App</h3>
+          <p>
+            <b>Summary:</b> A quiz game made with React that integrates the Open
+            Quiz API
+          </p>
+          <button>Demo</button>
+          <button>Code</button>
+        </div>
+      </div>
     </div>
   );
 }
