d9b6eacecf0c76a3c6f603e7776edd11456be818
austingw <senortinman@gmail.com>
2022-10-26 11:50:36 -0400

    Add cool box shadows to project images

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,15 +6,14 @@ export default function Portfolio() {
       <h2>Portfolio</h2>
       <div className="portfolio-item">
         <div className="project-summary">
+          <h3>Project: Quiz App</h3>
           <b>Summary</b>
           <p>A quiz game made with React that integrates the Open Quiz API</p>
           <button>Demo</button>
           <button>Code</button>
         </div>
         <div className="project-display">
-          <div className="img-bg">
-            <img src={quiz} className="project-img" />
-          </div>
+          <img src={quiz} className="project-img right-img" />
         </div>
       </div>
       <div className="portfolio-item-reverse"></div>
