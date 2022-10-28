502691684d6fe6d09736207d47b9be05bf2fe534
austingw <senortinman@gmail.com>
2022-10-28 13:49:30 -0400

    First draft of about content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,20 @@ export default function About() {
         <div className="section-title">
           <h2>About Me</h2>
         </div>
-        <p>Blah blah blah I'm Austin</p>
+        <p>My name is Austin and I'm a Frontend Dev!</p>
+        <p>
+          I've always had a passion for research, problem solving, and creative
+          expression. From making Wordpress sites for family friends in middle
+          school to soldering together guitar electronics, planning, testing and
+          troubleshooting are simply a way of life for me. Web development has
+          given me the opportunity to use my natural dispotion to craft new
+          experiences, conveint features, and responsive designs for both myself
+          and clients.
+        </p>
+        <p>
+          Technologies I use include React, Javascript, CSS, SASS, Bootstrap,
+          Vite, and Webpack, but I'm always excited to learn something new.
+        </p>
       </div>
       <div>
         <img src="" />
