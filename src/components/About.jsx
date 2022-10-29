bf69be08a77d6becb9133e230ec3582b020060eb
austingw <senortinman@gmail.com>
2022-10-29 19:16:34 -0400

    About section edits

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,15 +10,16 @@ export default function About() {
         <div className="about-wrapper">
           <img src={quiz} />
           <div className="about-content">
-            <p>My name is Austin and I'm a Frontend Dev!</p>
+            <p>👋 Hi, I’m Austin Williams, a Frontend Developer!</p>
             <p>
-              I've always had a passion for research, problem solving, and
-              creative expression. From making Wordpress sites for family
-              friends in middle school to soldering together guitar electronics,
-              testing and troubleshooting are simply a way of life for me. Web
-              development has given me the opportunity to use my natural
-              inclinations and attributes to craft new experiences, conveint
-              features, and responsive designs for both myself and clients.
+              For as long as I can remember, I've possessed a passion for
+              research, problem solving, and creative expression. In middle
+              school I taught myself how to build websites with Wordpress. Last
+              weekend I spent hours learning how to solder guitar electronics! I
+              love web development because it has granted me the fulfilling
+              opportunity to use my inclinations to craft new experiences,
+              conveint features, and responsive designs for both myself and
+              clients.
             </p>
             <p>
               Technologies I use include React, Javascript, CSS, SASS,
